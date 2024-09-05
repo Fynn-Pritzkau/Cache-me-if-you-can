d93adef16392d0ed1cee98876d4c8c4d705ac3e4
Fynn <126359947+Fynn-Pritzkau@users.noreply.github.com>
2024-09-05 09:31:23 +0200

    Initial Commit

---
+++ src/main/java/de/bbk/cachemeifyoucan/CacheMeIfYouCanApplication.java
@@ -0,0 +1,13 @@
+package de.bbk.cachemeifyoucan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheMeIfYouCanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheMeIfYouCanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/bbk/cachemeifyoucan/CacheMeIfYouCanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bbk.cachemeifyoucan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheMeIfYouCanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
