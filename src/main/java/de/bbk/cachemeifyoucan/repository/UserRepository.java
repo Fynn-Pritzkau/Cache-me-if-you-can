36454ffd441c76dc51a11453aadf9134ea8f009c
jonas.mizrahi <mizrahijoni@gmail.com>
2024-09-12 09:39:11 +0200

    removed unused classes for handling local database ->fixed pom dependencies

--- src/main/java/de/bbk/cachemeifyoucan/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.bbk.cachemeifyoucan.repository;
-
-import de.bbk.cachemeifyoucan.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long>
-{
-    User findByUsername(String username);
-}
