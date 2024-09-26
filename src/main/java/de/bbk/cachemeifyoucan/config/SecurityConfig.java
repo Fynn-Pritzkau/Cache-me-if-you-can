90e900121d57071180d918fe8116d4edaeb5fd79
fynn.pritzkau <fynnpritzkau@gmail.com>
2024-09-26 08:40:15 +0200

    Updated SecurityConfig to meet the requirements of the Vue Frontend

--- src/main/java/de/bbk/cachemeifyoucan/config/SecurityConfig.java
+++ src/main/java/de/bbk/cachemeifyoucan/config/SecurityConfig.java
@@ -4,15 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**").allowedOrigins("http://localhost:9191");
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login", "/api/logout").permitAll()
                         .anyRequest().authenticated())
